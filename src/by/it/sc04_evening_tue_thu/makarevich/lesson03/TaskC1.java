b6c202ccb6ad7b474fc2601531e4e7d650a49dcf
TMakarevich <tporazhinskaya@gmail.com>
2017-10-03 21:22:08 +0300

    Task A Lesson 3 complete

--- src/by/it/sc04_evening_tue_thu/makarevich/lesson03/TaskC1.java
+++ src/by/it/sc04_evening_tue_thu/makarevich/lesson03/TaskC1.java
@@ -9,16 +9,8 @@ package by.it.sc04_evening_tue_thu.makarevich.lesson03;
 Вывод:
 Sum = 26
 */
-import java.util.Scanner;
-
 class TaskC1 {
-    public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
-                 int i=sc.nextInt();
-                 int a=sc.nextInt();
-                 int Sum=i+a;
-        System.out.println("Sum"+" "+"="+" "+Sum);
-    }
+
 
 
 }
