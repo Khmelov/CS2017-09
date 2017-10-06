262170a51ec2254aca47da45fbc840423abc4ae0
Lyudatut13 <lyuda_tutina@mail.ru>
2017-10-06 16:02:26 +0300

    Full A, full B, partly C (C1,C2 exactly)

--- src/by/it/sc03_evening_mon_fri/tutina/lesson03/TaskB3.java
+++ src/by/it/sc03_evening_mon_fri/tutina/lesson03/TaskB3.java
@@ -10,13 +10,13 @@ package by.it.sc03_evening_mon_fri.tutina.lesson03;
 */        
 class TaskB3 {
         public static void main(String[] args) {
-//        String s = "C";
-//        System.out.println("Новым Годом");
+        String s = "C";
+        System.out.println("Новым Годом");
 //        System.out.println("C Новым");
 //        System.out.println("Годом");
 //        System.out.print("C Новым");
 //        System.out.println("Годом");
-//        System.out.println(s);
+        System.out.println(s);
 //        System.out.print("C Новым!");
 //        System.out.print(" ");
 //        System.out.println("Годом");
--- src/by/it/sc03_evening_mon_fri/tutina/lesson03/TaskC1.java
+++ src/by/it/sc03_evening_mon_fri/tutina/lesson03/TaskC1.java
@@ -9,8 +9,16 @@ package by.it.sc03_evening_mon_fri.tutina.lesson03;
 Вывод:
 Sum = 26
 */
+import java.util.Scanner;
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int i=sc.nextInt();
+        int j=sc.nextInt();
+        int k=i+j;
+        System.out.println("Sum = "+k);
 
+    }
 
 
 }
