27ebf88adb9a85c81c3c24428416359856d1462e
shinkevich <vadimeno@yandex.ru>
2017-10-31 20:01:01 +0300

    L10-AB

--- src/by/it/sc04_evening_tue_thu/shinkevich/lesson10/B1Sheduler.java
+++ src/by/it/sc04_evening_tue_thu/shinkevich/lesson10/B1Sheduler.java
@@ -44,12 +44,19 @@ public class B1Sheduler {
         List<Event> result;
         result = new ArrayList<>();
         //ваше решение.
-System.out.println(Arrays.toString(events));
-       Arrays.sort(events);
+        int i=0;                              //i - это индекс события events[i]
+        int n=events.length;
         System.out.println(Arrays.toString(events));
+        Arrays.sort(events);                                     //подготовка к жадному поглощению массива событий
+
+        while (i<n) {
 
 
-
+            //  while (i < n && events[i] <=stop) {
+            i++;
+        }
+           Arrays.sort(events);
+        System.out.println(Arrays.toString(events));
 
         return result;                        //вернем итог
     }
