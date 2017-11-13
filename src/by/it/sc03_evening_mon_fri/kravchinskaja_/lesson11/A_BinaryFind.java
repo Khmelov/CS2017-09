82b3f245e1641e59822c27ffb2f5ee9dac11dda8
Marry <krawchinskaja@gmail.com>
2017-11-13 18:30:36 +0300

    A and B

--- src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson11/A_BinaryFind.java
+++ src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson11/A_BinaryFind.java
@@ -46,15 +46,51 @@ public class A_BinaryFind {
         int k = scanner.nextInt();
         int[] result=new int[k];
         for (int i = 0; i < k; i++) {
+            result[i]=-1;
             int value = scanner.nextInt();
             //тут реализуйте бинарный поиск индекса
             //для значения value в саммиве a
             //результат поиска сохраните в result[i]
 
+            int leftIndex = 0;
+            int rightIndex = a.length - 1;
+
+            while (leftIndex < rightIndex) {
+
+                int middleIndex = (leftIndex + rightIndex) / 2;
+
+                if (a[middleIndex] == value) {
+
+                    result[i] = middleIndex + 1;
+
+                }
+                if (a[leftIndex] == value) {
+
+                    result[i] = leftIndex + 1;
+
+                }
+                if (a[rightIndex] == value) {
+
+                    result[i] = rightIndex + 1;
+
+                }
+
+                if (value > a[middleIndex]) {
+
+                    leftIndex = middleIndex + 1;
+
+                } else {
+
+                    rightIndex  = middleIndex - 1;
+
+                }
+
+            }
 
 
 
-            result[i]=-1;
+
+
         }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
@@ -74,3 +110,4 @@ public class A_BinaryFind {
     }
 
 }
+
