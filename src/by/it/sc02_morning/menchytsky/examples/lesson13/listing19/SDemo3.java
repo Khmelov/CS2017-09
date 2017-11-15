897d2e2bf5f751607678f9106b61e76b9be9bbc4
Stepan <s.menchytsky@gmail.com>
2017-11-16 00:23:21 +0300

    lesson13 B complete

---
+++ src/by/it/sc02_morning/menchytsky/examples/lesson13/listing19/SDemo3.java
@@ -0,0 +1,13 @@
+package by.it.sc02_morning.menchytsky.examples.lesson13.listing19;
+
+class SDemo3 {
+  public static void main(String args[]) { 
+    StaticBlock ob = new StaticBlock("Inside Constructor");
+ 
+    System.out.println("Square root of 2 is " +  
+                       StaticBlock.rootOf2); 
+    System.out.println("Square root of 3 is " +  
+                       StaticBlock.rootOf3); 
+ 
+  } 
+}
