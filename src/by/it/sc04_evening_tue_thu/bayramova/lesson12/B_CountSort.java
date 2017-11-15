66e821c066dada30160a891228a4123bd2ef09ee
Sabina <sb.sabina00@gmail.com>
2017-11-15 20:28:10 +0300

    Lesson 12

---
+++ src/by/it/sc04_evening_tue_thu/bayramova/lesson12/B_CountSort.java
@@ -0,0 +1,59 @@
+package by.it.sc04_evening_tue_thu.bayramova.lesson12;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Первая строка содержит число 1<=n<=10000, вторая - n натуральных чисел, не превышающих 10.
+Выведите упорядоченную по неубыванию последовательность этих чисел.
+
+При сортировке реализуйте метод с вычислительной сложностью не более, чем O(n)
+*/
+
+public class B_CountSort {
+
+
+    int[] countSort(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        int[] points = new int[n];
+
+        //читаем точки
+        for (int i = 0; i < n; i++) {
+            points[i] = scanner.nextInt();
+        }
+        //тут реализуйте логику задачи с применением сортировки подсчетом
+        int[] numbers = new int[10];
+        for (int point : points) {
+            numbers[point - 1] += 1;
+        }
+        int count = 0;
+        for (int i = 0; i < numbers.length; ++i) {
+            for (int j = 0; j < numbers[i]; ++j) {
+                points[count] = i+1;
+                count+=1;
+            }
+        }
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return points;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/akhmelev/lesson12/dataB.txt");
+        B_CountSort instance = new B_CountSort();
+        int[] result = instance.countSort(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+}
