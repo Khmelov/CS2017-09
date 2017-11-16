2181fb8ffb38dca8172f3e6a74400d5a7d854a03
V6767404 <32267531+V6767404@users.noreply.github.com>
2017-11-16 14:49:49 +0300

    Lesson 13 (B)

--- src/by/it/sc04_evening_tue_thu/yanushkevich/lesson13/TaskB1.java
+++ src/by/it/sc04_evening_tue_thu/yanushkevich/lesson13/TaskB1.java
@@ -1,4 +1,9 @@
 package by.it.sc04_evening_tue_thu.yanushkevich.lesson13;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 Напишите отдельный класс Salary который принимает в конструктор
 массив double[]
@@ -12,8 +17,8 @@ package by.it.sc04_evening_tue_thu.yanushkevich.lesson13;
 метод double[] getSalary(double percent) возвращает новый массив с надбавкой в percent%.
 метод void setSalary(double percent) обновляет массив в приватном поле с надбавкой в percent%.
 
-метод void sort() сортирует массив через Arrays.sort в приватном поле.
-метод String toString() возвращает Arrays.toString для в приватного поля
+    метод void sort() сортирует массив через Arrays.sort в приватном поле.
+    метод String toString() возвращает Arrays.toString для в приватного поля
 
  в методе main
  с консоли вводите заработные платы в массив чисел типа double размером в 12 элементов
@@ -29,4 +34,32 @@ package by.it.sc04_evening_tue_thu.yanushkevich.lesson13;
 */
 public class TaskB1 {
 
+
+    public static void main(String[] args) {
+
+        double[] mas = new double[12];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 12; i++) {
+            mas[i] = sc.nextDouble();
+            //    mas[i] = (int) (Math.random() * 88) + 10; // [10;99] (max - min + 1) + min
+        }
+
+      /*неотсортированные зарплаты без надбавок
+        сумму без надбавок.
+        неотсортированные зарплаты с надбавкой в 20%
+        сумму с этой надбавкой.
+        отсортированные зарплаты с надбавкой в 50%
+        сумму с этой надбавкой.*/
+
+        Salary sal = new Salary(mas);
+        System.out.println(Arrays.toString(sal.getSalary()));
+        System.out.println(sal.getSum());
+        System.out.println(Arrays.toString(sal.getSalary(20)));
+        System.out.println(sal.getSum(20));
+        sal.setSalary(50);
+        sal.sort();
+        System.out.println(Arrays.toString(sal.getSalary()));
+        System.out.println(sal.getSum());
+        // System.out.println(sal.getSalary()); почему-то не срабатывает переопределенный метод toString
+    }
 }
