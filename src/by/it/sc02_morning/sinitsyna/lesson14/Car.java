585ad2a2ffa382c6aa7da0bfc737c422722fdf9e
marina <sma_86@list.ru>
2017-11-20 18:58:21 +0300

    Lesson 14: A

---
+++ src/by/it/sc02_morning/sinitsyna/lesson14/Car.java
@@ -0,0 +1,4 @@
+package by.it.sc02_morning.sinitsyna.lesson14;
+
+public class Car {
+}
---
+++ src/by/it/sc02_morning/sinitsyna/lesson14/Maz.java
@@ -0,0 +1,9 @@
+package by.it.sc02_morning.sinitsyna.lesson14;
+
+public class Maz extends Car {
+
+    public Maz(int speed) {
+
+       // super((speed>80)?80:speed);
+    }
+}
