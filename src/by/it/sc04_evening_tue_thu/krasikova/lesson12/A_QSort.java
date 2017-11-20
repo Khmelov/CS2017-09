0bedd755d4e223f92dc7ec2327739e69883dde08
jiayu <yaotiao94@gmail.com>
2017-11-20 23:26:25 +0300

    Lesson 12 A (lesson 13 is not finished yet!)

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson12/A_QSort.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson12/A_QSort.java
@@ -47,14 +47,33 @@ import java.util.Scanner;
 
 public class A_QSort {
 
+    private class Event implements Comparable<Event>  {
+        int time;
+        int type;
+
+        public  Event(int time, int type) {
+            this.time = time;
+            this.type = type;
+        }
+
+        @Override
+        public int compareTo(Event otherEvent) {
+            int res=this.time-otherEvent.time;
+            if(res==0) res = -this.type + otherEvent.type;
+            return res;}
+
+        @Override
+        public String toString() {return "e("+ time + ", " + type + ") ";}
+    }
+
     //отрезок
     private class Segment  implements Comparable<Segment>{
         int start;
         int stop;
 
         Segment(int start, int stop){
-            this.start = start;
-            this.stop = stop;
+            this.start=start;
+            this.stop=stop;
             //тут можно доделать конструктор на случай если
             //концы отрезков придут в обратном порядке
         }
@@ -65,13 +84,34 @@ public class A_QSort {
             //и нужен ли он вообще.
             return 0;
         }
-
         @Override
         public String toString() {
-            return "("+start +":" + stop +')';
-        }
+            return "("+start +":" + stop +')';}
     }
+    private int partition(int[] a, int left, int right) {
+        int x=a[left];
+        int j=left;
+        for (int i=left+1; i<=right; i++) {
+            if (a[i] <= x) {
+                j++;
+                int tmp=a[j];
+                a[i]=tmp;
+            }
+        }
+        int tmp=a[j];
+        a[j]=a[left];
+        a[left]=tmp;
+        return j;}
 
+    private void qSort (int[] a, int left, int right) {
+        if (left>=right) return;
+        int m=partition (a, left, right);
+        qSort(a, left, m-1);
+        qSort(a, m+1, right);}
+
+    private  void qSort (int[] a) {
+        qSort(a, 0, a.length -1);
+    }
 
     int[] getAccessory(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
@@ -85,23 +125,37 @@ public class A_QSort {
         int[] points=new int[m];
         int[] result=new int[m];
 
+        Event[] events=new Event[2*n + m];
+        int counterEvent=0;
+
         //читаем сами отрезки
         for (int i = 0; i < n; i++) {
             //читаем начало и конец каждого отрезка
             segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
+            events[counterEvent++] = new Event(segments[i].start, 1);
+            events[counterEvent++] = new Event(segments[i].stop, -1);
+
         }
         System.out.println("segments="+ Arrays.toString(segments));
         //читаем точки
         for (int i = 0; i < m; i++) {
             points[i]=scanner.nextInt();
+            events[counterEvent++] = new Event(points[i], 0);
         }
         System.out.println("points="+ Arrays.toString(points));
         //тут реализуйте логику задачи
         //ОБЯЗАТЕЛЬНО с применением быстрой сортировки
-
-
-
-
+        Arrays.sort(events);
+        System.out.println("Sorted events=" + Arrays.toString(events));
+        int p=0;
+        int camOn=0;
+        for (int i = 0; i < events.length ; i++) {
+            if(events[i].type == 0) {
+                result[p++] = camOn;
+            } else {
+                camOn = camOn+events[i].type;}
+        }
+        System.out.println("Sorted points=" + Arrays.toString(points));
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
@@ -110,9 +164,8 @@ public class A_QSort {
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
         InputStream stream = new FileInputStream(root + "by/it/akhmelev/lesson12/dataA.txt");
-        A_QSort instance = new A_QSort();
+        A_QSort instance=new A_QSort();
         int[] result=instance.getAccessory(stream);
-        System.out.println("result="+ Arrays.toString(result));
-    }
+        System.out.println("result="+ Arrays.toString(result));}
 
 }
