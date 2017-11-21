34966a7c81b8830a2985d4d4fba2647b3b3b0af6
MikhailTov <Mensk84@gmail.com>
2017-11-21 11:13:19 +0300

    lesson 14: B

--- src/by/it/sc04_evening_tue_thu/Tovstik/lesson14/Car.java
+++ src/by/it/sc04_evening_tue_thu/Tovstik/lesson14/Car.java
@@ -1,27 +1,13 @@
 package by.it.sc04_evening_tue_thu.Tovstik.lesson14;
 
-public class Car {
+public class Car extends AbstractCar {
 
     private int speed;
 
     public Car(int speed) {
-        this.speed = speed;
-    }
-
-    void start() {
-        System.out.println(this + " поехал со скоростью " + this.speed + " км/ч");
-    }
-
-    void stop() {
-        System.out.println(this + " остановился");
-    }
-
-    void beep() {
-        System.out.println(this + " сигналит: Пи-бип!");
-    }
-
-    @Override
-    public String toString() {
-        return "Автомобиль";
+        super(speed);
     }
 }
+
+
+
