9a8a009c7564d8f0f4957c9f24c97f519cc308f3
Александр Хмелев <Khmelov@users.noreply.github.com>
2017-11-21 20:15:20 +0300

    Update TaskA1.java

--- src/by/it/sc04_evening_tue_thu/bayramova/lesson07/TaskA1.java
+++ src/by/it/sc04_evening_tue_thu/bayramova/lesson07/TaskA1.java
@@ -28,19 +28,19 @@ import java.util.Scanner;
 
 public class TaskA1 {
     public static void main(String[] args) {
-        int a[] = new int[10];
+        int a[]=new int[10];
         int i;
-        Scanner sc = new Scanner(System.in);
-        for (i = 0; i < 10; i++) {
-            a[i] = sc.nextInt();
+         Scanner sc=new Scanner(System.in);
+         for (i=0; i<10; i++) {
+             a[i]=sc.nextInt();
+         }
+        for (i=0; i<a.length/2; i++) {
+            int tmp=a[i];
+            a[i]=a[a.length-i-1];
+            a[a.length-i-1]=tmp;
         }
-        for (i = 0; i < a.length / 2; i++) {
-            int tmp = a[i];
-            a[i] = a[a.length - i - 1];
-            a[a.length - i - 1] = tmp;
-        }
-        for (i = 0; i < a.length; i++) {
+        for (i=0; i<a.length; i++) {
             System.out.println(a[i]);
         }
     }
-}
+    }
