a422452099c46387a9ef14e85d0264544e79e965
Alexey Tokun <alexeytokun@gmail.com>
2017-11-21 12:37:22 +0300

    Урок 14, A1-B1.

---
+++ src/by/it/sc04_evening_tue_thu/tokun/lesson14/Mercedes.java
@@ -0,0 +1,23 @@
+package by.it.sc04_evening_tue_thu.tokun.lesson14;
+
+public class Mercedes extends Car {
+    Mercedes(int speed) {
+        super((speed <= 200)?speed:200 );
+    }
+
+    @Override
+    void beep() {
+        System.out.println(this + " сигналит: Фа-фа!");
+    }
+
+    @Override
+    public String toString() {
+        return "Mercedes";
+    }
+
+    @Override
+    void fire() {
+        System.out.println(this + " включил зажигание");
+    }
+}
+
